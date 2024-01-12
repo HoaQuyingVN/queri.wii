88ae5e4372cde1f2caba6aec547ccdb2413eb829
1six <30725155-1six@users.noreply.replit.com>
2024-01-12 13:20:26 +0000

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+  
+</script>
+
+<template>
+  <main>
+    <h2>Welcome to Queri</h2>
+  </main>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url';
+import OhVueIcon from "oh-vue-icons";
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    include: ['oh-vue-icons']
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  }
+});
